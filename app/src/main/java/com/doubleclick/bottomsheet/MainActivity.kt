caf78544b9d5982de36c2e5b5cb217d8c864f0e2
eslam <eslamfgazy600@gmail.com>
2022-04-02 23:17:24 +0200

    kotlin-android-extensions

--- app/src/main/java/com/doubleclick/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/doubleclick/bottomsheet/MainActivity.kt
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sheet: FrameLayout = findViewById(R.id.sheet);
+//        val sheet: FrameLayout = findViewById(R.id.sheet);
         BottomSheetBehavior.from(sheet).apply {
             peekHeight = 200
             this.state = BottomSheetBehavior.STATE_COLLAPSED
